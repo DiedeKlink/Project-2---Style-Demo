29479ba873a055c5e02cff04ed7765fadc4a6937
DiedeKlink <diede@protonmail.com>
2024-07-29 11:50:51 +0200

    first commit

---
+++ components/ElevatedCards.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import React from 'react'
+
+export default function ElevatedCards() {
+  return (
+    <View>
+      <Text style={styles.headingText}>Elevated Cards</Text>
+      <ScrollView>
+
+      </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    headingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8
+    },
+})
